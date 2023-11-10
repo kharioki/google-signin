5e377109560d8c95dbd254654567e38eafdd3d92
Tony Kharioki <antolisah@gmail.com>
2023-11-11 00:26:19 +0300

    google signin app

--- App.js
+++ App.js
@@ -1,10 +1,78 @@
+import React, { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState(null);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "597506008726-6o96vl45ikjdhopvki8023n8fdcsb71q.apps.googleusercontent.com",
+    iosClientId: "597506008726-nu2e3e07ktrrbq8b0tmust11l5blge1i.apps.googleusercontent.com",
+    webClientId: "597506008726-1uk6es2a0fnmfkofcugvlhupoa2l7eiv.apps.googleusercontent.com"
+  })
+
+  useEffect(() => {
+    handleSignInWithGoogle();
+  }, [response])
+
+  async function handleSignInWithGoogle() {
+    const user = await AsyncStorage.getItem('@user');
+    if (!user) {
+      if (response?.type === 'success') {
+        await getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(JSON.parse(user));
+    }
+  }
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // handle error
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!userInfo ? (
+        <Button
+          title="Sign in with Google"
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+      ) : (
+        <View style={styles.card}>
+          {userInfo?.picture && (
+            <Image source={{ uri: userInfo?.picture }} style={styles.image} />
+          )}
+          <Text style={styles.text}>Email: {userInfo.email}</Text>
+          <Text style={styles.text}>
+            Verified: {userInfo.verified_email ? "yes" : "no"}
+          </Text>
+          <Text style={styles.text}>Name: {userInfo.name}</Text>
+          {/* <Text style={styles.text}>{JSON.stringify(userInfo, null, 2)}</Text> */}
+        </View>
+      )}
+      <Button title="delete local store" onPress={() => AsyncStorage.removeItem('@user')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +84,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  card: {
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 15,
+    marginBottom: 15,
+    alignItems: "center",
+    gap: 6,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
